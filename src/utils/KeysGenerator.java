c9e4d25de5747f204be9ad011734fd370243e69a
champCAR <gagoudechris@gmail.com>
2017-10-11 14:48:57 +0100

    save and load methods

--- src/utils/KeysGenerator.java
+++ src/utils/KeysGenerator.java
@@ -1,10 +1,17 @@
 package utils;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 public class KeysGenerator {
 	private KeyPair keyPair;
@@ -29,6 +36,25 @@ public class KeysGenerator {
 	
 	public PrivateKey getPrivateKeyFromKeyPair(){
 		return keyPair.getPrivate();
+	}
+	
+	public static void savePrivateKey(String clientName, String path, PrivateKey privateKey) throws IOException{
+		FileOutputStream fileOutputStream = new FileOutputStream(path+"/"+clientName+".txt");
+		fileOutputStream.write(privateKey.getEncoded());
+		fileOutputStream.close();
+	}
+	
+	public static PrivateKey loadPrivateKey(String clientName, String path) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException{
+		File filePrivateKey = new File(path+"/"+clientName+".txt");
+		FileInputStream fileInputStream = new FileInputStream(path+"/"+clientName+".txt");
+		
+		byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+		fileInputStream.read(encodedPrivateKey);
+		fileInputStream.close();
+		
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		return keyFactory.generatePrivate(privateKeySpec);
 	}
 	
 	public KeyPair getKeyPair() {
